935351e1ed4aee962ee1f6d7411bee7f3ae6a7d2
benmizrahi <benm@item-soft.com>
2015-11-10 23:00:19 +0200

    Adding files

---
+++ LightInfiniteScrollDirective.ts
@@ -0,0 +1,32 @@
+﻿ /// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../globalobjects/interfaces/isrollservice.ts" />
+
+module Global.Directives {
+
+    export class LightInfiniteScroll implements ng.IDirective {
+
+        constructor(private DataProvider: KeshetAnan.Interfaces.IScrollService) {
+        }
+
+        restrict = 'A';
+        link = (scope:ng.IScope, element, attrs) => {
+            var threshold = 100;
+            element.scroll(() => {
+                var visibleHeight = element.height();
+                if (this.DataProvider.NoMoreDetails)
+                    return;
+                var scrollableHeight = element.prop('scrollHeight');
+                var hiddenContentHeight = scrollableHeight - visibleHeight;
+                if (hiddenContentHeight - element.scrollTop() <= threshold) {
+                    scope.$apply(scope.$parent['ScrollAction']);
+                }
+            });
+        };
+
+         static factory(dataServiceNamne) {
+             var directive = (DataProvider: KeshetAnan.Interfaces.IScrollService) => new LightInfiniteScroll(DataProvider);
+             directive.$inject = [dataServiceNamne];
+            return directive;
+        }
+    }
+}
